0cf50ca6fe733e2cfeef8954bbade5d0c72efe08
Theo Kempf <theokempf@protonmail.com>
2025-04-08 15:30:42 +0200

    Train class flake8

--- Train.py
+++ Train.py
@@ -1,14 +1,23 @@
 from Agent import Agent
-import numpy as np
 from time import sleep
 import pygame
 from pynput.keyboard import Key, Listener
 import threading
 
 TILE_SIZE = 50
+title = "Learn2Slither"
+wall = "sprites/wall.png"
+s = "sprites/snake.png"
+head = "sprites/head.png"
+red = "sprites/red.png"
+green = "sprites/green.png"
+
 
 class Train:
-    def __init__(self, num_episodes=1000, qtable_filename = "snake_q_table.npy", decay=0.995, agent= Agent(), rate = 0, is_ui_on= False):
+    def __init__(self, num_episodes=1000,
+                 qtable_filename="snake_q_table.npy",
+                 decay=0.995, agent=Agent(), rate=0,
+                 is_ui_on=False):
         self.num_episodes = num_episodes
         self.qtable_filename = qtable_filename
         self.decay = decay
@@ -19,19 +28,24 @@ class Train:
         self.is_ui_on = is_ui_on
         if is_ui_on:
             pygame.init()
-            self.screen = pygame.display.set_mode((TILE_SIZE * 12, TILE_SIZE * 12))
+            self.screen = pygame.display.set_mode((TILE_SIZE * 12,
+                                                   TILE_SIZE * 12))
             pygame.display.set_caption('Learn2Slither')
         else:
-            self.t1 = threading.Thread(target=self.listen_for_keys, daemon=True)
+            self.t1 = threading.Thread(target=self.listen_for_keys,
+                                       daemon=True)
             self.listener = None
-    
+
     def train(self):
         """
         Training loop for the Snake Q-learning agent
-        
+
         Parameters:
-        - get_state_function: Function that returns the current state of the game
-        - perform_action_function: Function that performs an action and returns (next_state, reward, done)
+        - get_state_function: Function that returns the
+            current state of the game
+        - perform_action_function: Function that
+            performs an action and
+            returns (next_state, reward, done)
         - num_episodes: Number of training episodes
 
         rate:   0 step-by-step
@@ -48,7 +62,10 @@ class Train:
                 break
             state = self.agent.get_state()
             episode_reward = 0
-            state_list, action_list, reward_list, next_state_list = [], [], [], []
+            state_list = []
+            action_list = []
+            reward_list = []
+            next_state_list = []
             while self.agent.board.death is False:
                 if self.is_running is False:
                     break
@@ -74,7 +91,8 @@ class Train:
                     sleep(1.5)
                 self.next_step = False
             for i in range(len(state_list)):
-                self.agent.update_q_value(state_list[i], action_list[i], reward_list[i], next_state_list[i])
+                self.agent.update_q_value(state_list[i], action_list[i],
+                                          reward_list[i], next_state_list[i])
             self.agent.decay_exploration()
             rewards_per_episode.append(episode_reward)
             self.agent.board.resurrect()
@@ -84,21 +102,20 @@ class Train:
         self.t1.join()
         self.agent.save_q_table(self.qtable_filename)
         return self.agent, rewards_per_episode
-    
+
     def train_ui(self):
         """
             Copy of train function with ui made with pygame.
         """
-        self.grass_sprite = pygame.image.load("sprites/grass.png").convert_alpha()
-        self.wall_sprite = pygame.image.load("sprites/wall.png").convert_alpha()
-        self.snake_sprite = pygame.image.load("sprites/snake.png").convert_alpha()
-        self.head_sprite = pygame.image.load("sprites/head.png").convert_alpha()
-        self.red_sprite = pygame.image.load("sprites/red.png").convert_alpha()
-        self.green_sprite = pygame.image.load("sprites/green.png").convert_alpha()
+        self.wall_sprite = pygame.image.load(wall).convert_alpha()
+        self.snake_sprite = pygame.image.load(s).convert_alpha()
+        self.head_sprite = pygame.image.load(head).convert_alpha()
+        self.red_sprite = pygame.image.load(red).convert_alpha()
+        self.green_sprite = pygame.image.load(green).convert_alpha()
         pygame.display.set_icon(self.head_sprite)
-        
+
         clock = pygame.time.Clock()
-        
+
         rewards_per_episode = []
         for episode in range(self.num_episodes):
             self.process_pygame_events()
@@ -106,7 +123,10 @@ class Train:
                 break
             state = self.agent.get_state()
             episode_reward = 0
-            state_list, action_list, reward_list, next_state_list = [], [], [], []
+            state_list = []
+            action_list = []
+            reward_list = []
+            next_state_list = []
             while self.agent.board.death is False:
                 self.process_pygame_events()
                 if not self.is_running:
@@ -126,7 +146,7 @@ class Train:
                 action_list.append(action)
                 reward_list.append(reward)
                 next_state_list.append(next_state)
-                
+
                 if self.rate == 0:
                     self.next_step = False
                     while self.rate == 0 and not self.next_step:
@@ -142,11 +162,12 @@ class Train:
             if not self.is_running:
                 break
             for i in range(len(state_list)):
-                self.agent.update_q_value(state_list[i], action_list[i], reward_list[i], next_state_list[i])
+                self.agent.update_q_value(state_list[i], action_list[i],
+                                          reward_list[i], next_state_list[i])
             self.agent.decay_exploration()
             rewards_per_episode.append(episode_reward)
             self.agent.board.resurrect()
-            
+
         self.is_running = False
         self.agent.save_q_table(self.qtable_filename)
         return self.agent, rewards_per_episode
@@ -173,9 +194,8 @@ class Train:
                     pos_y = y * TILE_SIZE
                     self.screen.blit(sprite, (pos_x, pos_y))
         pygame.display.flip()
-    
+
     def process_pygame_events(self):
-        """Process Pygame events to keep the window responsive and handle key presses"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.is_running = False
@@ -190,7 +210,7 @@ class Train:
                     print("Exiting...")
                     self.is_running = False
                     return
-    
+
     def listen_for_keys(self):
         def on_release(key):
             if key == Key.space:
