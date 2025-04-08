bc7b934151fa1e6118ce59a6560d9f0a5c3bf94a
Theo Kempf <theokempf@protonmail.com>
2025-04-08 13:51:47 +0200

    snake.py refactor to comply with flake8 norm

--- snake.py
+++ snake.py
@@ -5,7 +5,8 @@ import os.path
 from Train import Train
 
 """
-    step-by-step avec entrer par défaut en fonction de la commande. Appuyer sur espace pour changer (->human readable -> computer speed).
+    step-by-step avec entrer par défaut en fonction de la commande.
+    Appuyer sur espace pour changer (->human readable -> computer speed).
 
     Entrainement de nouveau model sans ui.
     Entrainement de nouveau model avec ui.
@@ -15,29 +16,39 @@ from Train import Train
     Démonstration model sans ui.
     Démonstration model avec ui.
 """
+
+
 def main():
-    parser = argparse.ArgumentParser(description="Example script with arguments.")
+    parser = argparse.ArgumentParser(description="Learn2Slither")
     parser.add_argument("--sessions", type=int, help="number of sessions")
     parser.add_argument("--mode", type=str, help="train or play a model")
-    parser.add_argument("--modelname", type=str, help="chose an existing model (npy file) to continue training or display")
-    parser.add_argument("--ui", type=str, help="<on/off> decides if want to display a game of the model")
-    parser.add_argument("--rate", type=str, help="<step/human/cpu> decides if want to train/play model step-by-step or human readable or computer speed")
+    parser.add_argument("--modelname", type=str, help="chose an existing model\
+                        (npy file) to continue training or display")
+    parser.add_argument("--ui", type=str, help="<on/off> decides if want to\
+                        display a game of the model")
+    parser.add_argument("--rate", type=str, help="<step/human/cpu> decides if\
+                        want to train/play model step-by-step or human\
+                        readable or computer speed")
 
     args = parser.parse_args()
-    
+
     rate_map = {"step": 0, "human": 1, "cpu": 2}
     rate = rate_map.get(args.rate, 0)
-    if os.path.isfile(args.modelname): #filename exist
-        if args.mode == "train": #continue training utiliser load_qtable
+    if os.path.isfile(args.modelname):
+        if args.mode == "train":
             agent = Agent()
-            agent.load_q_table(args.modelname)# tester en entrainant un model 500x puis 1 fois
+            agent.load_q_table(args.modelname)
             if args.sessions > 0:
                 if args.ui == "on":
-                    t = Train(num_episodes=args.sessions, qtable_filename=args.modelname, agent= agent, rate= rate, is_ui_on= True)
+                    t = Train(num_episodes=args.sessions,
+                              qtable_filename=args.modelname,
+                              agent=agent, rate=rate, is_ui_on=True)
                 else:
-                    t = Train(num_episodes=args.sessions, qtable_filename=args.modelname, agent= agent, rate= rate, is_ui_on= False)
+                    t = Train(num_episodes=args.sessions,
+                              qtable_filename=args.modelname,
+                              agent=agent, rate=rate, is_ui_on=False)
                 t.train()
-        elif args.mode == "play": #make model play without training it
+        elif args.mode == "play":
             if args.ui == "on":
                 g = Play(rate=rate, is_ui_on=True)
                 g.display_gameplay_ui(args.modelname)
@@ -46,35 +57,34 @@ def main():
                 g.display_gameplay_term(args.modelname)
         else:
             print_error()
-    elif os.path.isfile(args.modelname) is False: #filename doesnt exist
-        if args.mode == "train": #create and train new model
+    elif os.path.isfile(args.modelname) is False:
+        if args.mode == "train":
             agent = Agent()
             if args.rate is None:
                 rate = 0
             if args.sessions > 0:
                 if (args.ui == "on"):
                     print("good")
-                    t = Train(num_episodes=args.sessions, qtable_filename=args.modelname, agent= agent, rate= rate, is_ui_on= True)
+                    t = Train(num_episodes=args.sessions,
+                              qtable_filename=args.modelname,
+                              agent=agent, rate=rate, is_ui_on=True)
                 else:
-                    t = Train(num_episodes=args.sessions, qtable_filename=args.modelname, agent= agent, rate= rate, is_ui_on= False)
+                    t = Train(num_episodes=args.sessions,
+                              qtable_filename=args.modelname,
+                              agent=agent, rate=rate, is_ui_on=False)
                 t.train()
         elif args.mode == "play":
             print("Error : Need an existing model to make it play")
             exit(0)
     else:
         print_error()
-                
+
 
 def print_error():
-    print("Usage: python3 snake.py --modelname <filename.npy> --mode <train/play> --sessions <int> --ui <on/off>")
+    print("Usage: python3 snake.py --modelname <filename.npy> \
+          --mode <train/play> --sessions <int> --ui <on/off>")
     exit(1)
+
 
 if __name__ == "__main__":
     main()
-
-"""
-python3 snake.py --ui on  --modelname testtt.npy --mode train --sessions 100 --rate step
-python3 snake.py --ui off  --modelname testtt.npy --mode train --sessions 1000 --rate step
-python3 snake.py --ui on  --modelname testtt.npy --mode play --rate step
-python3 snake.py --ui off  --modelname void.npy --mode play --rate cpu
-"""
